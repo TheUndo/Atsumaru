1f5b28a69cdb4dbc2fc1d14606eb361aa5eb7bea
pixelfog <pixelfog@tutanota.com>
2022-02-27 14:45:11 +0530

    Transparency should be disabled when page fill method is full width and background color is anything other than black and gray

--- frontend/src/pages/read/desktopChapterNavigation/DesktopChapterNavigation.tsx
+++ frontend/src/pages/read/desktopChapterNavigation/DesktopChapterNavigation.tsx
@@ -10,135 +10,140 @@ import { ReaderContext } from "../Reader";
 import classes from "./desktopChapterNavigation.module.scss";
 
 export default function DesktopChapterNavigation() {
-    const {
-        currentChapter,
-        currentPage,
-        chapters,
-        jumpChapter,
-        pageContentScrollPosition,
-    } = useContext(ReaderContext);
-    const [{ readingDirection, readerKeyboardNavigation }] = useContext(
-        AppContext
-    ).settings ?? [{}];
-    const desktop = useMedia(["(pointer: fine)"], [true], true);
+  const {
+    currentChapter,
+    currentPage,
+    chapters,
+    jumpChapter,
+    pageContentScrollPosition,
+  } = useContext(ReaderContext);
+  const [
+    {
+      readingDirection,
+      readerKeyboardNavigation,
+      imageFitMethod,
+      readerBackgroundColor,
+    },
+  ] = useContext(AppContext).settings ?? [{}];
+  const desktop = useMedia(["(pointer: fine)"], [true], true);
 
-    const currentChapterIndex = chapters.findIndex(
-        (chapter) => chapter.name === currentChapter?.name
-    );
-    const nextChapter = chapters[currentChapterIndex - 1];
-    const previousChapter = chapters[currentChapterIndex + 1];
+  const currentChapterIndex = chapters.findIndex(
+    chapter => chapter.name === currentChapter?.name,
+  );
+  const nextChapter = chapters[currentChapterIndex - 1];
+  const previousChapter = chapters[currentChapterIndex + 1];
 
-    const reverse = useMemo(
-        () => readingDirection === "RIGHT-TO-LEFT",
-        [readingDirection]
-    );
+  const reverse = useMemo(
+    () => readingDirection === "RIGHT-TO-LEFT",
+    [readingDirection],
+  );
 
-    const [cPage] = useMemo(
-        () => parsePageUrlParameter(currentPage ?? "-1"),
-        [currentPage]
-    );
-    const nextShortcut =
-        readerKeyboardNavigation?.nextPage === "YES" &&
-        readerKeyboardNavigation.disabled !== "YES";
-    const previousShortcut =
-        readerKeyboardNavigation?.previousPage === "YES" &&
-        readerKeyboardNavigation.disabled !== "YES";
+  const [cPage] = useMemo(
+    () => parsePageUrlParameter(currentPage ?? "-1"),
+    [currentPage],
+  );
+  const nextShortcut =
+    readerKeyboardNavigation?.nextPage === "YES" &&
+    readerKeyboardNavigation.disabled !== "YES";
+  const previousShortcut =
+    readerKeyboardNavigation?.previousPage === "YES" &&
+    readerKeyboardNavigation.disabled !== "YES";
 
-    const lastPage =
-        currentChapter?.pages?.[currentChapter.pages.length - 1]?.name ?? "-2";
-    const firstPage = currentChapter?.pages?.[0]?.name ?? "-2";
+  const lastPage =
+    currentChapter?.pages?.[currentChapter.pages.length - 1]?.name ?? "-2";
+  const firstPage = currentChapter?.pages?.[0]?.name ?? "-2";
 
-    const pageEl = useMemo(() => document.getElementById("pageContent"), []);
+  const pageEl = useMemo(() => document.getElementById("pageContent"), []);
 
-    const nextHidden = useMemo(
-        () =>
-            lastPage !== cPage ||
-            (readingDirection === "TOP-TO-BOTTOM" &&
-                (pageEl?.scrollHeight ?? 0) - (pageEl?.clientHeight ?? 0) >=
-                    (pageContentScrollPosition ?? 0) + 300),
-        [cPage, pageContentScrollPosition, pageEl, lastPage]
-    );
-    const previousHidden = firstPage !== cPage;
+  const nextHidden = useMemo(
+    () =>
+      lastPage !== cPage ||
+      (readingDirection === "TOP-TO-BOTTOM" &&
+        (pageEl?.scrollHeight ?? 0) - (pageEl?.clientHeight ?? 0) >=
+          (pageContentScrollPosition ?? 0) + 300),
+    [cPage, pageContentScrollPosition, pageEl, lastPage],
+  );
+  const previousHidden = firstPage !== cPage;
 
-    if (!desktop) return <></>;
+  if (!desktop) return <></>;
 
-    return (
-        <>
-            {previousChapter && (
-                <Navigation
-                    onClick={() => jumpChapter(-1)}
-                    button={
-                        <Button
-                            transparent
-                            className={classes.button}
-                            icon={
-                                <Icon
-                                    icon="arrow"
-                                    orientation={
-                                        !reverse ? "-.25turn" : ".25turn"
-                                    }
-                                />
-                            }
-                            iconLoc={!reverse ? "left" : "right"}
-                        >
-                            Chapter {previousChapter.name}{" "}
-                            {previousShortcut && "(P)"}
-                        </Button>
-                    }
-                    className={cm(
-                        !reverse ? classes.left : classes.right,
-                        previousHidden && classes.hidden
-                    )}
+  const transparencyDisabled = useMemo(
+    () =>
+      imageFitMethod === "TO-WIDTH" ||
+      !["#000000", "#111"].includes(readerBackgroundColor!),
+    [imageFitMethod, readerBackgroundColor],
+  );
+
+  return (
+    <>
+      {previousChapter && (
+        <Navigation
+          onClick={() => jumpChapter(-1)}
+          button={
+            <Button
+              transparent={!transparencyDisabled}
+              className={classes.button}
+              icon={
+                <Icon
+                  icon="arrow"
+                  orientation={!reverse ? "-.25turn" : ".25turn"}
                 />
-            )}
-            {nextChapter && (
-                <Navigation
-                    onClick={() => jumpChapter(1)}
-                    button={
-                        <Button
-                            transparent
-                            className={classes.button}
-                            icon={
-                                <Icon
-                                    icon="arrow"
-                                    orientation={
-                                        reverse ? "-.25turn" : ".25turn"
-                                    }
-                                />
-                            }
-                            iconLoc={reverse ? "left" : "right"}
-                        >
-                            Chapter {nextChapter.name} {nextShortcut && "(N)"}
-                        </Button>
-                    }
-                    className={cm(
-                        reverse ? classes.left : classes.right,
-                        nextHidden && classes.hidden
-                    )}
+              }
+              iconLoc={!reverse ? "left" : "right"}>
+              Chapter {previousChapter.name} {previousShortcut && "(P)"}
+            </Button>
+          }
+          className={cm(
+            !reverse ? classes.left : classes.right,
+            previousHidden && classes.hidden,
+          )}
+        />
+      )}
+      {nextChapter && (
+        <Navigation
+          onClick={() => jumpChapter(1)}
+          button={
+            <Button
+              transparent={!transparencyDisabled}
+              className={classes.button}
+              icon={
+                <Icon
+                  icon="arrow"
+                  orientation={reverse ? "-.25turn" : ".25turn"}
                 />
-            )}
-        </>
-    );
+              }
+              iconLoc={reverse ? "left" : "right"}>
+              Chapter {nextChapter.name} {nextShortcut && "(N)"}
+            </Button>
+          }
+          className={cm(
+            reverse ? classes.left : classes.right,
+            nextHidden && classes.hidden,
+          )}
+        />
+      )}
+    </>
+  );
 }
 
 function Navigation({
-    button,
-    className,
-    onClick,
+  button,
+  className,
+  onClick,
 }: {
-    button: React.ReactNode;
-    className: string;
-    onClick: () => void;
+  button: React.ReactNode;
+  className: string;
+  onClick: () => void;
 }) {
-    const [ripple, event] = useRipple();
+  const [ripple, event] = useRipple();
 
-    return (
-        <>
-            <div onClick={onClick} className={cm(className, classes.item)}>
-                <div className={classes.inner} ref={ripple} onMouseDown={event}>
-                    {button}
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div onClick={onClick} className={cm(className, classes.item)}>
+        <div className={classes.inner} ref={ripple} onMouseDown={event}>
+          {button}
+        </div>
+      </div>
+    </>
+  );
 }

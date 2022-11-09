5a593f47bb481dbfda087d552410f59a951407fd
Undo <42096971+TheUndo@users.noreply.github.com>
2022-11-09 18:10:36 +0100

    fix all broken images

--- scraper/src/mangaSee123.com/imageProber.ts
+++ scraper/src/mangaSee123.com/imageProber.ts
@@ -14,49 +14,50 @@ export default async function imageProber() {
     const doc = (await cursor.next()) as MangaSee.MangaInfo;
 
     if (doc) {
-      const chapter = doc.chapters[0];
-      if (chapter) {
-        const page = chapter.pages[0];
-        if (page) {
-          try {
-            const responses = await Promise.all(
-              page.pageURLs.map(async url => {
-                try {
-                  return await axios.head(url);
-                } catch (e) {
-                  return { status: 404 };
-                }
-              }),
-            );
-
-            if (
-              !responses.every(
-                response => response.status >= 200 && response.status < 300,
-              )
-            ) {
-              log(
-                "[PROBER]: image failed! FAILURE",
-                doc._id,
-                page.pageURLs.toString(),
+      for (const chapter of doc?.chapters ?? []) {
+        if (chapter) {
+          const page = chapter.pages[0];
+          if (page) {
+            try {
+              const responses = await Promise.all(
+                page.pageURLs.map(async url => {
+                  try {
+                    return await axios.head(url);
+                  } catch (e) {
+                    return { status: 404 };
+                  }
+                }),
               );
-              const mangaEntry = await scrapeInfoPage(doc.mangaSeeSlug, true);
-              if (mangaEntry) {
-                /* manga.push(mangaEntry); */
-                try {
-                  // causes db side effect
-                  void (await upsertMangaSee(mangaEntry));
-                } catch (e) {
-                  console.error(e);
-                  void log("Error when upserting", mangaEntry.title);
+
+              if (
+                !responses.every(
+                  response => response.status >= 200 && response.status < 300,
+                )
+              ) {
+                log(
+                  "[PROBER]: image failed! FAILURE",
+                  doc._id,
+                  page.pageURLs.toString(),
+                );
+                const mangaEntry = await scrapeInfoPage(doc.mangaSeeSlug, true);
+                if (mangaEntry) {
+                  /* manga.push(mangaEntry); */
+                  try {
+                    // causes db side effect
+                    void (await upsertMangaSee(mangaEntry));
+                  } catch (e) {
+                    console.error(e);
+                    void log("Error when upserting", mangaEntry.title);
+                  }
                 }
+                sleep(1000);
+              } else {
+                //log("[PROBER]: image succeeded! :)", page.pageURLs.toString());
+                sleep(300);
               }
-              sleep(1000);
-            } else {
-              //log("[PROBER]: image succeeded! :)", page.pageURLs.toString());
-              sleep(300);
+            } catch (e) {
+              log("[PROBER]", e);
             }
-          } catch (e) {
-            log("[PROBER]", e);
           }
         }
       }
